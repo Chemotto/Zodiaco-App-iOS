1d272840708d66aeb2fba346654d3d6864eeb099
tardes <jmo.gonzalez@gmail.com>
2026-01-19 20:38:19 +0100

    finalización aplicación

--- Zodiaco-App-iOS/ViewControllers/DetailViewController.swift
+++ Zodiaco-App-iOS/ViewControllers/DetailViewController.swift
@@ -8,34 +8,39 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     // MARK: Outlets
-
+    
     @IBOutlet weak var iconImageView: UIImageView!
-
+    
     @IBOutlet weak var FavoriteButtonIcon: UIBarButtonItem!
-
+    
+    @IBOutlet weak var PredictionTextView: UITextView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
     // MARK: Properties
-
+    
     var horoscope: Horoscopo!
     var session: SessionManager = SessionManager()
     var isFavorite: Bool = false
-
+    
     // MARK: Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-
+        
         navigationItem.title = horoscope.name
         navigationItem.subtitle = horoscope.dates
-
+        
         iconImageView.image = horoscope.getIcon()
-
+        
         isFavorite = session.isfavorite(id: horoscope.id)
         setFavoriteIcon()
-
+        
     }
     
     func setFavoriteIcon() {
@@ -45,7 +50,17 @@ class DetailViewController: UIViewController {
             FavoriteButtonIcon.image = UIImage(systemName: "heart")
         }
     }
-
+    
+    
+    // Mark: Actions
+    
+    
+    @IBAction func didChangePeriod(_ sender: UISegmentedControl) {
+    }
+    
+    
+    
+    
     @IBAction func setFavorite(_ sender: Any) {
         isFavorite = !isFavorite
         if isFavorite {
@@ -56,8 +71,27 @@ class DetailViewController: UIViewController {
         
         setFavoriteIcon()
     }
-
-    @IBAction func share(_ sender: Any) {
-
+    
+    @IBAction func share(_ sender: Any){
+        
+        
+        
+        
+        func getHoroscopePrediction() {
+            let url = URL(string: "https://itune.apple.com/")
+            
+            guard let url = url else {
+                print("invalid url")
+                return
+            }
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+            }
+                
+            
+                
+            }
+            
+        }
     }
-}
+    
